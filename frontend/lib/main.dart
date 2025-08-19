08d6cdcfbb00062f0ecd25f701e7b731ee2d377e
jeremy <thejeremyjohn@gmail.com>
2025-08-19 11:45:33 -0400

    added sortable DataTable to wip homepage with temporary data

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import 'dart:math'; // TODO rm
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,11 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, // TODO
-      title: 'Flutter Demo',
-      // theme: ThemeData(
-      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      // ),
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: const MyHomePage(title: 'wellread'),
     );
@@ -25,15 +23,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -41,25 +30,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // Generate a list of fiction prodcts
+  // placeholder from https://www.kindacode.com/article/flutter-datatable
+  final List<Map> _products = List.generate(30, (i) {
+    return {"id": i, "name": "Product $i", "price": Random().nextInt(200) + 1};
+  });
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  int _currentSortColumn = 0;
+  bool _isAscending = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // backgroundColor: const Color.fromARGB(255, 250, 248, 246),
         backgroundColor: Colors.brown,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -115,38 +98,59 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SizedBox(
+        width: double.infinity,
+        child: SingleChildScrollView(
+          child: DataTable(
+            sortColumnIndex: _currentSortColumn,
+            sortAscending: _isAscending,
+            headingRowColor: WidgetStateProperty.all(Colors.amber[200]),
+            columns: [
+              sortableDataColumn('ID', 'id'),
+              sortableDataColumn('Name', 'name'),
+              sortableDataColumn('Price', 'price'),
+            ],
+            rows: _products.map((item) {
+              return DataRow(
+                cells: [
+                  DataCell(Text(item['id'].toString())),
+                  DataCell(Text(item['name'])),
+                  DataCell(Text(item['price'].toString())),
+                ],
+              );
+            }).toList(),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  DataColumn sortableDataColumn(String labelText, String sortKey) {
+    return DataColumn(
+      label: Text(
+        labelText,
+        style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+      ),
+      onSort: (columnIndex, _) {
+        setState(() {
+          _currentSortColumn = columnIndex;
+          if (_isAscending == true) {
+            _isAscending = false;
+            // sort the product list in Ascending, order by sortKey
+            _products.sort(
+              (productA, productB) =>
+                  productB[sortKey].compareTo(productA[sortKey]),
+            );
+          } else {
+            _isAscending = true;
+            // sort the product list in Descending, order by sortKey
+            _products.sort(
+              (productA, productB) =>
+                  productA[sortKey].compareTo(productB[sortKey]),
+            );
+          }
+        });
+      },
     );
   }
 }
